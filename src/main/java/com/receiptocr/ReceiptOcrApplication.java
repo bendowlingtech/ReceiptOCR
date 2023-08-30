e8f5448a6d96c897d1091bded51f58f3308ff229
Ben Dowling <bendowlingtech@gmail.com>
2023-08-30 23:11:54 +0100

    initial

---
+++ src/main/java/com/receiptocr/ReceiptOcrApplication.java
@@ -0,0 +1,13 @@
+package com.receiptocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptOcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/receiptocr/ReceiptOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receiptocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
